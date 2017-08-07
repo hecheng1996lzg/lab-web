32bb4336c2307da86a79e1f59d571abdf9eb9e37
hecheng1996lzg <lens96@foxmail.com>
2017-08-07 23:41:32 +0800

    修改team的前后端

--- app/Visitor.php
+++ app/Visitor.php
@@ -6,5 +6,40 @@ use Illuminate\Database\Eloquent\Model;
 
 class Visitor extends Model
 {
-    //
+    private $indexArr;
+
+    private $yearLayout = '';
+
+    private $contentLayout = '<li class="{{index}}">
+                            <img src="{{photo}}" alt="">
+                            <strong>姓名：{{name}}</strong>
+                            <span>研究方向：{{research}}</span>
+                            <span>联系方式：{{contact}}</span>
+                            </li>';
+
+    public function getHtml($year,$indexArr){
+        $this->indexArr = $indexArr;
+        $year = $year? $year:date('Y');
+
+        $contents = $this->all();
+        $contentHtml = $this->getContentHtml($contents);
+
+        return ['content'=>$contentHtml,
+            'year'=>''];
+    }
+
+    private function getContentHtml($contents){
+        $html = '';
+        foreach ($contents as $key=>$content){
+            $indexId = $key<3? $this->indexArr[$key]:'';
+            $row = $this->contentLayout;
+            $row = str_replace('{{index}}',$indexId,$row);
+            $row = str_replace('{{photo}}',$content->photo,$row);
+            $row = str_replace('{{name}}',$content->name,$row);
+            $row = str_replace('{{research}}',$content->research,$row);
+            $row = str_replace('{{contact}}',$content->contact,$row);
+            $html.=$row;
+        }
+        return $html;
+    }
 }
